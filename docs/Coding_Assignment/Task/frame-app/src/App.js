f5d51f067cf0865b221c3121a82ea147424c24d1
rodigb <rodix08@gmail.com>
2022-07-19 19:31:17 +0100

    dropzone implemented

--- docs/Coding_Assignment/Task/frame-app/src/App.js
+++ docs/Coding_Assignment/Task/frame-app/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Navbar from './components/navbar'
+import Screen from './pages/screen'
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Screen/>
       
     </div>
   );
--- docs/Coding_Assignment/Task/frame-app/src/pages/screen.js
+++ docs/Coding_Assignment/Task/frame-app/src/pages/screen.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import './screen.scss'
 
-function screen() {
+import {useDropzone} from 'react-dropzone'
+
+
+
+
+
+function Screen1() {
+    //dropzone const values
+    const {getRootProps, getInputProps, isDragActive} = useDropzone();
+
+
   return (
-    <div>screen</div>
+
+    //dropzone code
+    <div className="screen1">
+        <div {...getRootProps({ className: "dropzone" })}>
+            <input id="fileItem" type="file1" className="input-zone" {...getInputProps()} />
+            {isDragActive ? (
+
+                <p className="dropzone-content">
+                    Release to drop the files here
+                </p>
+                ) : (
+                <p className="dropzone-content">
+                    Drag and drop the files of a sequence to extract the metadata
+                </p>
+                )}
+        </div>
+    </div>
   )
+
 }
 
-export default screen
+
+
+export default Screen1
